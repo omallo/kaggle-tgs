3860142425d3424e8a2a574b8e012d04521ea1ad
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-14 19:54:48 +0200

    added a script to run the training on paperspace

--- paperspace.sh
+++ paperspace.sh
@@ -1,8 +1,13 @@
+#!/usr/bin/env bash
+
 apt-get update
 apt-get -y install python3-dev libsm-dev libxrender1 libxext6
+rm -rf /var/lib/apt/lists/*
+
 pip install virtualenv
 virtualenv env --python=python3
 . env/bin/activate
+
 pip install -r requirements.txt
+
 python train.py
-rm -rf /var/lib/apt/lists/*
