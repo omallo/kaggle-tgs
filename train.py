71a2b150847371c8acbc2ab6866c398b5024eb34
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-16 23:29:56 +0200

    initialize the weights of the swa model to match the ones of the training model

--- train.py
+++ train.py
@@ -292,6 +292,7 @@ val_set_y = val_set_df.masks.tolist()
 # model = UNet(in_depth=3, out_depth=1, base_channels=32).to(device)
 model = AlbuNet(pretrained=True).to(device)
 swa_model = AlbuNet(pretrained=True).to(device)
+swa_model.load_state_dict(model.state_dict())
 # model.load_state_dict(torch.load("/storage/albunet.pth"))
 
 criterion = nn.BCEWithLogitsLoss()
