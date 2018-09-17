6bbfc6c5a60e982dee91809259e54d4c8f3b64ec
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-17 14:10:04 +0200

    update the swa model at the end of every clr cycle

--- train.py
+++ train.py
@@ -321,7 +321,7 @@ clr_cycle_size = 2 * clr_step_size
 clr_scale_fn = lambda x: 1.0 / (1.1 ** (x - 1))
 clr_iterations = 0
 
-swa_c_epochs = 1
+swa_c_epochs = 4
 swa_n = 0
 
 optimizer = optim.Adam(model.parameters(), lr=clr_base_lr)
