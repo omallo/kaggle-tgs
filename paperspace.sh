35f78326088fa4bf47cc12b859691bd879802508
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-17 15:28:14 +0200

    install the zip tool

--- paperspace.sh
+++ paperspace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get -y install python3-dev libsm-dev libxrender1 libxext6
+apt-get -y install python3-dev libsm-dev libxrender1 libxext6 zip
 rm -rf /var/lib/apt/lists/*
 
 pip install virtualenv
