a16557c2810e4d0f1ca26e55dbead3845e74c78d
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-18 15:03:11 +0200

    only train images with coverage class 1

--- train.py
+++ train.py
@@ -276,7 +276,7 @@ def main():
     train_df["coverage_class"] = train_df.coverage.map(coverage_to_class)
 
     # TODO: remove this again
-    train_df = train_df.drop(train_df.index[(train_df.coverage_class < 1) | (train_df.coverage_class > 3)])
+    train_df = train_df.drop(train_df.index[(train_df.coverage_class != 1) | (train_df.coverage_class != 3)])
 
     train_df["contours"] = train_df.masks.map(contour)
     train_df["mask_weights"] = [calculate_mask_weights(m) for m, c in zip(train_df.masks, train_df.coverage_class)]
