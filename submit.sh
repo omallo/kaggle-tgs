bf983ec4aebeed48674ac6117f70a1f0df53a5a0
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-19 18:54:43 +0200

    run apt-get/pip quietly

--- submit.sh
+++ submit.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-apt-get -qq update
-apt-get -qq -y install python3-dev libsm-dev libxrender1 libxext6 zip
+apt-get update >/dev/null
+apt-get -y install python3-dev libsm-dev libxrender1 libxext6 zip >/dev/null
 rm -rf /var/lib/apt/lists/*
 
-pip install virtualenv
+pip -q --no-color install virtualenv
 virtualenv env --python=python3
 . env/bin/activate
 
-pip install -r requirements.txt
+pip -q --no-color install -r requirements.txt
 
 python submit.py | tee -a /artifacts/out.log
