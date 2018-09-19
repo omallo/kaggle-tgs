262402101ee2f6cab72c94464bac7dcbd027cc5d
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-19 20:36:42 +0200

    write the git hash to the log file

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update >/dev/null
-apt-get -y install python3-dev libsm-dev libxrender1 libxext6 zip >/dev/null
+apt-get -y install python3-dev libsm-dev libxrender1 libxext6 zip git >/dev/null
 rm -rf /var/lib/apt/lists/*
 
 pip -q install virtualenv
@@ -9,6 +9,8 @@ virtualenv env --python=python3
 . env/bin/activate
 
 pip -q install -r requirements.txt
+
+echo "commit: $(git rev-parse HEAD)" >/artifacts/out.log
 
 python train.py | tee -a /artifacts/out.log
 
