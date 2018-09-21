f0503e2d5a7b8448e60e2172598b7884fa5ca8e6
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-21 15:30:40 +0200

    fine tuning

--- dataset.py
+++ dataset.py
@@ -38,11 +38,11 @@ class TrainDataset(Dataset):
         self.augment = augment
 
     def __len__(self):
-        return len(self.df)
+        return 2 * len(self.df)
 
     def __getitem__(self, index):
-        image = self.df.images[index]
-        mask = self.df.masks[index]
+        image = self.df.images[index % len(self.df)]
+        mask = self.df.masks[index % len(self.df)]
 
         if self.augment:
             image, mask = augment(image, mask)
