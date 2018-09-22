be62530586d8f207eec5bdfe902efbd43794291b
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-22 13:24:00 +0200

    use a resnet34 based model from neptun.ml

--- models.py
+++ models.py
@@ -4,117 +4,114 @@ from torch import nn
 from torch.nn import functional as F
 
 
-def conv3x3(in_, out):
-    return with_he_normal_weights(nn.Conv2d(in_, out, 3, padding=1))
-
-
-class ConvRelu(nn.Module):
-    def __init__(self, in_: int, out: int):
+class ConvBnRelu(nn.Module):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.conv = conv3x3(in_, out)
-        self.activation = nn.ReLU(inplace=True)
+        self.conv = nn.Sequential(nn.Conv2d(in_channels, out_channels, 3, padding=1),
+                                  nn.BatchNorm2d(out_channels),
+                                  nn.ReLU(inplace=True)
+                                  )
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.activation(x)
+        return self.conv(x)
+
+
+class DecoderBlockV2(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels, is_deconv=True):
+        super(DecoderBlockV2, self).__init__()
+        self.is_deconv = is_deconv
+
+        self.deconv = nn.Sequential(
+            ConvBnRelu(in_channels, middle_channels),
+            nn.ConvTranspose2d(middle_channels, out_channels, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+
+        self.upsample = nn.Sequential(
+            ConvBnRelu(in_channels, out_channels),
+            nn.Upsample(scale_factor=2, mode='bilinear'),
+        )
+
+    def forward(self, x):
+        if self.is_deconv:
+            x = self.deconv(x)
+        else:
+            x = self.upsample(x)
         return x
 
 
-class DecoderBlock(nn.Module):
-    def __init__(self, in_channels, middle_channels, out_channels, is_deconv=True):
-        super(DecoderBlock, self).__init__()
-        self.in_channels = in_channels
-
-        if is_deconv:
-            self.block = nn.Sequential(
-                ConvRelu(in_channels, middle_channels),
-                nn.ConvTranspose2d(middle_channels, out_channels, kernel_size=4, stride=2,
-                                   padding=1),
-                nn.ReLU(inplace=True)
-            )
-        else:
-            self.block = nn.Sequential(
-                nn.Upsample(scale_factor=2, mode='bilinear'),
-                ConvRelu(in_channels, middle_channels),
-                ConvRelu(middle_channels, out_channels),
-            )
-
-    def forward(self, x):
-        return self.block(x)
-
-
-class AlbuNet34(nn.Module):
+class UNetResNet(nn.Module):
+    """PyTorch U-Net model using ResNet(34, 101 or 152) encoder.
+    UNet: https://arxiv.org/abs/1505.04597
+    ResNet: https://arxiv.org/abs/1512.03385
+    Proposed by Alexander Buslaev: https://www.linkedin.com/in/al-buslaev/
+    Args:
+            encoder_depth (int): Depth of a ResNet encoder (34, 101 or 152).
+            num_classes (int): Number of output classes.
+            num_filters (int, optional): Number of filters in the last layer of decoder. Defaults to 32.
+            dropout_2d (float, optional): Probability factor of dropout layer before output layer. Defaults to 0.2.
+            pretrained (bool, optional):
+                False - no pre-trained weights are being used.
+                True  - ResNet encoder is pre-trained on ImageNet.
+                Defaults to False.
+            is_deconv (bool, optional):
+                False: bilinear interpolation is used in decoder.
+                True: deconvolution is used in decoder.
+                Defaults to False.
     """
-        UNet (https://arxiv.org/abs/1505.04597) with Resnet34(https://arxiv.org/abs/1512.03385) encoder
-        Proposed by Alexander Buslaev: https://www.linkedin.com/in/al-buslaev/
-        """
 
-    def __init__(self, num_classes=1, num_filters=32, pretrained=False, is_deconv=False):
-        """
-        :param num_classes:
-        :param num_filters:
-        :param pretrained:
-            False - no pre-trained network is used
-            True  - encoder is pre-trained with resnet34
-        :is_deconv:
-            False: bilinear interpolation is used in decoder
-            True: deconvolution is used in decoder
-        """
+    def __init__(self, encoder_depth, num_classes, num_filters=32, dropout_2d=0.2,
+                 pretrained=False, is_deconv=False):
         super().__init__()
         self.num_classes = num_classes
+        self.dropout_2d = dropout_2d
+
+        if encoder_depth == 34:
+            self.encoder = torchvision.models.resnet34(pretrained=pretrained)
+            bottom_channel_nr = 512
+        elif encoder_depth == 101:
+            self.encoder = torchvision.models.resnet101(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        elif encoder_depth == 152:
+            self.encoder = torchvision.models.resnet152(pretrained=pretrained)
+            bottom_channel_nr = 2048
+        else:
+            raise NotImplementedError('only 34, 101, 152 version of Resnet are implemented')
 
         self.pool = nn.MaxPool2d(2, 2)
 
-        self.encoder = torchvision.models.resnet34(pretrained=pretrained)
-
         self.relu = nn.ReLU(inplace=True)
 
-        self.conv1 = nn.Sequential(self.encoder.conv1,
-                                   self.encoder.bn1,
-                                   self.encoder.relu,
-                                   self.pool)
+        self.input_adjust = nn.Sequential(self.encoder.conv1,
+                                          self.encoder.bn1,
+                                          self.encoder.relu)
 
-        self.conv2 = self.encoder.layer1
+        self.conv1 = self.encoder.layer1
+        self.conv2 = self.encoder.layer2
+        self.conv3 = self.encoder.layer3
+        self.conv4 = self.encoder.layer4
 
-        self.conv3 = self.encoder.layer2
-
-        self.conv4 = self.encoder.layer3
-
-        self.conv5 = self.encoder.layer4
-
-        self.center = DecoderBlock(512, num_filters * 8 * 2, num_filters * 8, is_deconv)
-
-        self.dec5 = DecoderBlock(512 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv)
-        self.dec4 = DecoderBlock(256 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv)
-        self.dec3 = DecoderBlock(128 + num_filters * 8, num_filters * 4 * 2, num_filters * 2, is_deconv)
-        self.dec2 = DecoderBlock(64 + num_filters * 2, num_filters * 2 * 2, num_filters * 2 * 2, is_deconv)
-        self.dec1 = DecoderBlock(num_filters * 2 * 2, num_filters * 2 * 2, num_filters, is_deconv)
-        self.dec0 = ConvRelu(num_filters, num_filters)
-        self.final = nn.Conv2d(num_filters, num_classes, kernel_size=1)
+        self.dec4 = DecoderBlockV2(bottom_channel_nr, num_filters * 8 * 2, num_filters * 8, is_deconv)
+        self.dec3 = DecoderBlockV2(bottom_channel_nr // 2 + num_filters * 8, num_filters * 8 * 2, num_filters * 8,
+                                   is_deconv)
+        self.dec2 = DecoderBlockV2(bottom_channel_nr // 4 + num_filters * 8, num_filters * 4 * 2, num_filters * 2,
+                                   is_deconv)
+        self.dec1 = DecoderBlockV2(bottom_channel_nr // 8 + num_filters * 2, num_filters * 2 * 2, num_filters * 2 * 2,
+                                   is_deconv)
+        self.final = nn.Conv2d(num_filters * 2 * 2, num_classes, kernel_size=1)
 
     def forward(self, x):
-        conv1 = self.conv1(x)
+        input_adjust = self.input_adjust(x)
+        conv1 = self.conv1(input_adjust)
         conv2 = self.conv2(conv1)
         conv3 = self.conv3(conv2)
-        conv4 = self.conv4(conv3)
-        conv5 = self.conv5(conv4)
-
-        center = self.center(self.pool(conv5))
-
-        dec5 = self.dec5(torch.cat([center, conv5], 1))
-
-        dec4 = self.dec4(torch.cat([dec5, conv4], 1))
+        center = self.conv4(conv3)
+        dec4 = self.dec4(center)
         dec3 = self.dec3(torch.cat([dec4, conv3], 1))
         dec2 = self.dec2(torch.cat([dec3, conv2], 1))
-        dec1 = self.dec1(dec2)
-        dec0 = self.dec0(dec1)
-
-        if self.num_classes > 1:
-            x_out = F.log_softmax(self.final(dec0), dim=1)
-        else:
-            x_out = self.final(dec0)
-
-        return x_out
+        dec1 = F.dropout2d(self.dec1(torch.cat([dec2, conv1], 1)), p=self.dropout_2d)
+        return self.final(dec1)
 
 
 def with_he_normal_weights(layer):
--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 
 from dataset import TrainData, TrainDataset
 from metrics import precision_batch
-from models import AlbuNet34
+from models import UNetResNet
 from utils import moving_parameter_average, get_learning_rate
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -63,10 +63,10 @@ def main():
     val_set = TrainDataset(train_data.val_set_df, image_size_target, augment=False)
     val_set_data_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=2)
 
-    model = AlbuNet34(num_filters=32, pretrained=True, is_deconv=True).to(device)
+    model = UNetResNet(34, 1, num_filters=32, dropout_2d=0.2, pretrained=True, is_deconv=False).to(device)
     # model.load_state_dict(torch.load("/storage/model.pth", map_location=device))
 
-    swa_model = AlbuNet34(num_filters=32, pretrained=True, is_deconv=True).to(device)
+    swa_model = UNetResNet(34, 1, num_filters=32, dropout_2d=0.2, pretrained=True, is_deconv=False).to(device)
     swa_model.load_state_dict(model.state_dict())
 
     print("train_set_samples: %d, val_set_samples: %d" % (len(train_set), len(val_set)))
