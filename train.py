34f36cfd3b1544cb21d1c7c8d59b0ae2232688d4
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-23 18:48:28 +0200

    switch both models to train mode before updating the swa model

--- train.py
+++ train.py
@@ -150,6 +150,9 @@ def main():
         swa_updated = False
         if epoch + 1 >= swa_start_epoch and (model_improved or (epoch + 1) % swa_cycle_epochs == 0):
             swa_update_count += 1
+            # TODO: is this necessary?
+            model.train()
+            swa_model.train()
             moving_parameter_average(swa_model, model, 1.0 / swa_update_count)
             swa_updated = True
 
