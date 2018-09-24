71b15f385e194816b14fea4062eab18cb213d4ea
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-09-24 13:15:50 +0200

    log swa model update parameters for troubleshooting

--- utils.py
+++ utils.py
@@ -20,6 +20,10 @@ def unfreeze(model):
 
 
 def moving_parameter_average(target_model, source_model, alpha):
+    print("moving average: alpha=%.3f" % alpha)
     for param1, param2 in zip(target_model.parameters(), source_model.parameters()):
+        print("moving average: param1_before=%.3f" % param1.data)
         param1.data *= (1.0 - alpha)
         param1.data += param2.data * alpha
+        print("moving average: param1_after=%.3f" % param1.data)
+        print("moving average: param2=%.3f" % param2.data)
