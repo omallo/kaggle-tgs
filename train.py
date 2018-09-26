d3b2b98c3fa96b2d23827fc09ebf2bff8ab92eb7
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-09-26 12:20:43 +0200

    freeze the resnet encoder for less epochs

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ def main():
     sgdr_cycle_epochs = 20
     sgdr_cycle_end_patience = 3
     train_abort_epochs_without_improval = 20
-    epoch_to_unfreeze_encoder = 20
+    epoch_to_unfreeze_encoder = 10
 
     model_dir = sys.argv[1] if len(sys.argv) > 1 else None
 
