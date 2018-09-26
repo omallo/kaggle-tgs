7ea6fb1fdb3a505bd55d365d88bf121b6e44287c
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-09-26 09:37:40 +0200

    use the original ReLU activation function for the resnet layers which use pretrained weights

--- se_models.py
+++ se_models.py
@@ -30,7 +30,7 @@ class SEBasicBlock(nn.Module):
         super(SEBasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ELU(inplace=True)
+        self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
         self.se = SELayer(planes, reduction)
