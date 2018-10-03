2089c427060e023963d8f78333872930872ac725
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-03 12:58:06 +0200

    reduced the sgdr cycle length

--- train.py
+++ train.py
@@ -85,12 +85,12 @@ def main():
     batch_size = 32
     epochs_to_train = 300
     bce_loss_weight_gamma = 0.98
-    sgdr_min_lr = 0.00005  # 0.0001, 0.001
+    sgdr_min_lr = 0.0001  # 0.0001, 0.001
     sgdr_max_lr = 0.001  # 0.001, 0.03
-    sgdr_cycle_epochs = 50
+    sgdr_cycle_epochs = 20
     sgdr_cycle_epoch_prolongation = 0
     sgdr_cycle_end_patience = 3
-    train_abort_epochs_without_improval = 55
+    train_abort_epochs_without_improval = 30
     ensemble_model_count = 3
     swa_epoch_to_start = 0
 
