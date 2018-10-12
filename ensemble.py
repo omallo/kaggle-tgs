5e03c1588f8056463abac7aac479dc7c89465ba4
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-12 22:42:47 +0200

    fixed the ensemble model

--- ensemble.py
+++ ensemble.py
@@ -10,8 +10,9 @@ class Ensemble(nn.Module):
     def forward(self, x):
         all_prediction_logits = [m(x) for m in self.models]
         sum_mask_predictions = torch.zeros_like(all_prediction_logits[0][0])
-        sum_has_salt_predictions = torch.zeros_like(all_prediction_logits[0][1])
+        # sum_has_salt_predictions = torch.zeros_like(all_prediction_logits[0][1])
         for prediction_logits in all_prediction_logits:
             sum_mask_predictions += torch.sigmoid(prediction_logits[0])
-            sum_has_salt_predictions += torch.sigmoid(prediction_logits[1])
-        return sum_mask_predictions / len(self.models), sum_has_salt_predictions / len(self.models)
+            # sum_has_salt_predictions += torch.sigmoid(prediction_logits[1])
+        # return sum_mask_predictions / len(self.models), sum_has_salt_predictions / len(self.models)
+        return sum_mask_predictions / len(self.models), None
