d20910e13aade0cf9bc885cba403dcb046d85f28
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-12 22:23:49 +0200

    fixed the ensemble model

--- ensemble.py
+++ ensemble.py
@@ -8,7 +8,7 @@ class Ensemble(nn.Module):
         self.models = models
 
     def forward(self, x):
-        all_predictions = [torch.sigmoid(m(x)) for m in self.models]
+        all_predictions = [torch.sigmoid(m(x)[0]) for m in self.models]
         sum_predictions = torch.zeros_like(all_predictions[0])
         for p in all_predictions:
             sum_predictions += p
