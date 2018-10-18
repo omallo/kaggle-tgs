d0211729c6fe9987d3c73c6f4a4c400fbefbce06
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-18 17:56:52 +0200

    train only on data with salt coverage class 1

--- dataset.py
+++ dataset.py
@@ -83,9 +83,6 @@ class TrainData:
         train_set_df = train_set_df.reset_index()
         val_set_df = val_set_df.reset_index()
 
-        train_set_df = train_set_df.drop(train_set_df.index[train_set_df.coverage_class != 1]).copy()
-        val_set_df = val_set_df.drop(val_set_df.index[val_set_df.coverage_class != 1]).copy()
-
         print()
         train_set_df["coverage_class"] = train_set_df.masks.map(calculate_coverage_class)
         print(train_set_df.groupby("coverage_class").agg({"coverage_class": "count"}))
@@ -93,6 +90,11 @@ class TrainData:
         val_set_df["coverage_class"] = val_set_df.masks.map(calculate_coverage_class)
         print(val_set_df.groupby("coverage_class").agg({"coverage_class": "count"}))
         print()
+
+        train_set_df = train_set_df.drop(train_set_df.index[train_set_df.coverage_class != 1]).copy()
+        val_set_df = val_set_df.drop(val_set_df.index[val_set_df.coverage_class != 1]).copy()
+        train_set_df = train_set_df.reset_index()
+        val_set_df = val_set_df.reset_index()
 
         self.train_set_df = train_set_df
         self.val_set_df = val_set_df
